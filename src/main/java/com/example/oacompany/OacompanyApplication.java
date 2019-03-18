26ce531daa0c5304e92df3427a9db8db91f32bc0
jumbuckr <zimu0910@163.com>
2019-03-18 11:22:45 +0800

    Initial commit

---
+++ src/main/java/com/example/oacompany/OacompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.oacompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OacompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OacompanyApplication.class, args);
+    }
+
+}
